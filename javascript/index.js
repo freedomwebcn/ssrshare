418e1a0665d303148a38d4c42f84f5d6e13fc66e
freedomwebcn <freedomweb@yeah.net>
2019-11-27 01:22:04 +0800

    usa

--- javascript/index.js
+++ javascript/index.js
@@ -7,7 +7,8 @@ function copyText() {
     var text = document.getElementById("copy_text").innerText;;
     var input = document.getElementById("copyVal");
     input.value = text; //修改文本框的内容
-    input.select(); // 选中文本
+    input.focus();
+    input.setSelectionRange(0, 9999); // 选中文本
     document.execCommand("copy"); //执行浏览器复制命令
     $("#copyVal").remove();
     alert("复制成功");
